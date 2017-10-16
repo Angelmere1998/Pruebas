ad3b590d7b5f956273ea157a2fc1b117d6e07596
Angelmere1998 <32834405+Angelmere1998@users.noreply.github.com>
2017-10-16 10:54:03 +0200

    Add files via upload

---
+++ TestAnimal.java
@@ -0,0 +1,23 @@
+﻿	import java.util.*;
+
+	public class TestAnimal {
+		
+	   public static void main(String[] args) {
+
+		   Animal animal1 = new Animal();
+
+		   Scanner teclado = new Scanner(System.in);
+
+		   System.out.print("Teclee el Nombre del Animal: ");
+
+		   animal1.setnombre(teclado.nextLine());
+
+		   System.out.print("Teclee la edad del animal: ");
+
+		   animal1.setEdad(edad);
+
+		   animal1.nace();
+		   System.out.println("Me llamo " + animal1.getNombre() +
+		                      " y tengo " + animal1.getEdad() + "años");
+	   }
+	}
